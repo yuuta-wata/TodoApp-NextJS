6fc17e1e98e6a8bce4a648ab55d442ac668745ad
yuuta <watanabe@unlog.co.jp>
2020-07-21 13:24:56 +0900

    Add Home Page

---
+++ pages/home.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import {GetServerSideProps} from 'next'
+
+import {DemoDate} from '../demoDate'
+
+import TimeLineCell from '../components/TimeLineCell'
+
+interface Props {
+    date: {
+      id: string
+      nickName: string
+      todoList: {
+        id: string
+        item: string
+      }[]
+    }[]
+  }
+
+export default function Home(props: Props) {
+    return (
+        <div>
+            <Link href='/'>
+                <a>Topへ戻る</a>
+            </Link>
+            <main>
+                <div className="timeLine">
+                    {props.date.map(user => (
+                        <TimeLineCell nickName={user.nickName} todoList={user.todoList} />
+                    ))}
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const res = DemoDate
+    return {
+        props: {
+            date: res
+        }
+    }
+}
