ca09822da44fec36a1ed3f244343235748040d87
yuuta <watanabe@unlog.co.jp>
2020-08-19 11:28:59 +0900

    Added Todo function

--- packages/my/graphql/generated/graphql.tsx
+++ packages/my/graphql/generated/graphql.tsx
@@ -98,6 +98,27 @@ export type DeleteAccountInput = {
   password: Scalars['String'];
 };
 
+export type CreateTodoMutationVariables = Exact<{
+  title: Scalars['String'];
+}>;
+
+
+export type CreateTodoMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'createTodo'>
+);
+
+export type GetTodoListQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetTodoListQuery = (
+  { __typename?: 'Query' }
+  & { getTodoList?: Maybe<Array<Maybe<(
+    { __typename?: 'Todo' }
+    & Pick<Todo, 'id' | 'userId' | 'title'>
+  )>>> }
+);
+
 export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -110,6 +131,70 @@ export type MeQuery = (
 );
 
 
+export const CreateTodoDocument = gql`
+    mutation CreateTodo($title: String!) {
+  createTodo(input: {title: $title})
+}
+    `;
+export type CreateTodoMutationFn = ApolloReactCommon.MutationFunction<CreateTodoMutation, CreateTodoMutationVariables>;
+
+/**
+ * __useCreateTodoMutation__
+ *
+ * To run a mutation, you first call `useCreateTodoMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateTodoMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createTodoMutation, { data, loading, error }] = useCreateTodoMutation({
+ *   variables: {
+ *      title: // value for 'title'
+ *   },
+ * });
+ */
+export function useCreateTodoMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CreateTodoMutation, CreateTodoMutationVariables>) {
+        return ApolloReactHooks.useMutation<CreateTodoMutation, CreateTodoMutationVariables>(CreateTodoDocument, baseOptions);
+      }
+export type CreateTodoMutationHookResult = ReturnType<typeof useCreateTodoMutation>;
+export type CreateTodoMutationResult = ApolloReactCommon.MutationResult<CreateTodoMutation>;
+export type CreateTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<CreateTodoMutation, CreateTodoMutationVariables>;
+export const GetTodoListDocument = gql`
+    query GetTodoList {
+  getTodoList {
+    id
+    userId
+    title
+  }
+}
+    `;
+
+/**
+ * __useGetTodoListQuery__
+ *
+ * To run a query within a React component, call `useGetTodoListQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetTodoListQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetTodoListQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetTodoListQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<GetTodoListQuery, GetTodoListQueryVariables>) {
+        return ApolloReactHooks.useQuery<GetTodoListQuery, GetTodoListQueryVariables>(GetTodoListDocument, baseOptions);
+      }
+export function useGetTodoListLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<GetTodoListQuery, GetTodoListQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<GetTodoListQuery, GetTodoListQueryVariables>(GetTodoListDocument, baseOptions);
+        }
+export type GetTodoListQueryHookResult = ReturnType<typeof useGetTodoListQuery>;
+export type GetTodoListLazyQueryHookResult = ReturnType<typeof useGetTodoListLazyQuery>;
+export type GetTodoListQueryResult = ApolloReactCommon.QueryResult<GetTodoListQuery, GetTodoListQueryVariables>;
 export const MeDocument = gql`
     query Me {
   me {
--- packages/my/pages/my/index.tsx
+++ packages/my/pages/my/index.tsx
@@ -1,20 +1,79 @@
-import { useMeQuery } from '../../graphql/generated/graphql'
+import { useState } from 'react'
+
+import {
+  useMeQuery,
+  useCreateTodoMutation,
+  useGetTodoListQuery
+} from '../../graphql/generated/graphql'
 
 export default function MyPage() {
-  const { data, loading, error } = useMeQuery()
+  const [task, setTask] = useState<string>('')
 
-  if (error) return <div>再度ログインしてください</div>
+  const {
+    data: myData,
+    loading: myDataLoading,
+    error: myDataError
+  } = useMeQuery()
 
-  if (loading) return <div>Loading...</div>
+  const [
+    createTodo,
+    { loading: todoLoading, error: todoError }
+  ] = useCreateTodoMutation()
+
+  const {
+    data: todoList,
+    loading: todoListLoading,
+    error: todoListError
+  } = useGetTodoListQuery({ pollInterval: 1000 })
+
+  if (myDataError) return <div>再度ログインしてください</div>
+
+  if (myDataLoading) return <div>Loading...</div>
 
   return (
     <div>
       <h2>マイページ</h2>
-      {data && data.me ? (
-        <div>ようこそ！{`${data.me.nickname}さん！`}</div>
+      {myData && myData.me ? (
+        <div>ようこそ！{`${myData.me.nickname}さん！`}</div>
       ) : (
         <div>データがありません</div>
       )}
+      <div>
+        {todoError && (
+          <p style={{ color: 'red' }}>タスクを追加出来ませんでした</p>
+        )}
+        <input
+          placeholder='タスク'
+          value={task}
+          onChange={e => setTask(e.target.value)}
+        />
+        <button
+          type='button'
+          onClick={async () => {
+            try {
+              await createTodo({
+                variables: {
+                  title: task
+                }
+              })
+              setTask('')
+            } catch {}
+          }}
+          disabled={todoLoading}
+        >
+          {todoLoading ? 'Loading...' : 'TODO!!!'}
+        </button>
+      </div>
+      <div>
+        {todoListLoading && <p>Loading...</p>}
+        {todoListError && <p>Error...</p>}
+        {todoList && todoList.getTodoList
+          ? todoList.getTodoList.map(list => {
+              if (!list) return <p>Error...</p>
+              return <p key={list.id}>{list.title}</p>
+            })
+          : null}
+      </div>
     </div>
   )
 }
