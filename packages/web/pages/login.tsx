8765d25651029184fafad366e3a4814a062dc842
yuuta <watanabe@unlog.co.jp>
2020-08-20 23:05:53 +0900

    add router

--- packages/web/pages/login.tsx
+++ packages/web/pages/login.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 import { useLoginMutation } from '../graphql/generated/graphql'
 import { LoginForm } from '../components/LoginForm'
@@ -6,10 +7,13 @@ import { LoginForm } from '../components/LoginForm'
 export default function Login() {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+
   const [login, { error, loading }] = useLoginMutation()
+  const router = useRouter()
 
   return (
     <>
+      <h2>ログイン</h2>
       <LoginForm
         emailValue={email}
         emailEvent={e => setEmail(e.target.value)}
@@ -18,8 +22,7 @@ export default function Login() {
         buttonPress={async () => {
           try {
             await login({ variables: { email, password } })
-            setEmail('')
-            setPassword('')
+            window.location.replace('http://localhost:3001/my')
           } catch {}
         }}
         isLoading={loading}
