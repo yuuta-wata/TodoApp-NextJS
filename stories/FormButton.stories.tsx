33da3b43c93dd75c61e24af28b3696a10632e25a
yuuta <watanabe@unlog.co.jp>
2020-08-21 13:51:59 +0900

    add argTypes and addon-actions

--- stories/FormButton.stories.tsx
+++ stories/FormButton.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Meta, Story } from '@storybook/react/types-6-0'
+import { action } from '@storybook/addon-actions'
 
 import { FormButton } from '../packages/web/components/FormButton'
 import { Props } from '../packages/web/components/FormButton'
@@ -10,11 +11,12 @@ export default {
   component: FormButton,
   decorators: [
     Story => (
-      <div style={{ margin: '3em', backgroundColor: 'pink' }}>
+      <div style={{ margin: '3em' }}>
         <Story />
       </div>
     )
-  ]
+  ],
+  argTypes: { onPress: { action: 'clicked' } }
 } as Meta
 
 // 公式に記載されているやり方
@@ -25,8 +27,7 @@ export const RegsterButton = Template.bind({})
 RegsterButton.args = {
   type: 'button',
   title: '登録',
-  isLoading: false,
-  onPress: () => {}
+  isLoading: false
 }
 
 export const LoginButton = Template.bind([])
@@ -34,5 +35,5 @@ LoginButton.args = {
   type: 'button',
   title: 'ログイン',
   isLoading: false,
-  onPress: () => {}
+  onPress: action('onPress')
 }
